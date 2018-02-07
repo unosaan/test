4793a3b2b9377ec1c5b077435e4224a6a36e99cb
testuser <unosaan@yahoo.co.jp>
2018-02-07 21:58:29 +0900

    ２月７日

--- 4eachblog3/script.js
+++ 4eachblog3/script.js
@@ -1,75 +1,20 @@
-var a1 = 2;
-if (a1 < 10) {
-    document.write("A");
+for(var i=1;i<=10; i++){
+    if(i%2==0){
+        document.write(i);
+    }
 }
 
-var a2 = 10;
-if (a2 <= 10) {
-    document.write("B");
-
+for(var a=3;a<=8;a++){
+    if(a>5){
+       document.write(a);
+    document.write("<br>");
+       }
 }
 
-var a1 = 2;
-if (a1 < 10) {
-    document.write("A");
+for(var b=1;b<=9;b++){
+    for(var c=1;c<=9;c++){
+        document.write(b*c);
+    }
+    document.write("→"+b+"の段<br>");
 }
 
-var a4 = 12;
-if (a4 == 12) {
-    document.write("同じ数字");
-
-}
-
-var a5 = "田中";
-if (a5 == "田中") {
-    document.write(a5 + "さんです");
-}
-
-var b1 = 100;
-if (b1 >= 100) {
-    document.write("100より小さい数字です");
-}
-
-var b2 = 3;
-if (b2 < 1) {
-    document.write("A");
-} else if (b2 > 1) {
-    document.write("b");
-
-}
-
-var b3 = 10;
-
-if (b3 < 10) {
-    document.write("X");
-} else if (b3 > 5) {
-    document.write("Y");
-}
-
-var b4 = 1;
-
-if (b4 == 0) {
-    document.write("数字は0");
-} else if (b4 == 1) {
-    document.write("数字は１");
-} else if (b4 == 2) {
-    document.write("数字は２");
-
-
-}
-
-var b5 = -8;
-
-if (b5 < 0) {
-    document.write("マイナス");
-} else if (b5 >= 1) {
-    document.write("プラス");
-}
-
-var c1 = 10;
-
-if (c1 < 10) {
-    document.write("★");
-} else {
-    document.write("☆");
-}
