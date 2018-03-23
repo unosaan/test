566599388c2a7024cbb1ffd4eb01885ade18373c
exyskns <freelancerelease@gmail.com>
2018-03-23 21:53:29 +0900

    3月23日

---
+++ HelloWorld/src/Person.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+
+/**
+ * @author testuser
+ *
+ */
+
+
+	/**
+	 * @param args
+	 */
+	public class Person {
+
+		public String name=null;
+		public int age=0;
+	public Person(){}
+
+	public Person(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+
+
+
+	}
+
---
+++ HelloWorld/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	public static void main(String[] args) {
+
+		Person p =new Person();
+
+	Person taro=new Person();
+	taro.name="taro";
+	taro.age=18;
+	System.out.println(taro.name);
+
+	}
+}
