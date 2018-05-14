7d8b7d40248ca5aa45685afc2d1fc0b7209bb807
tatsuhitomoriya <tmj4oc3s_q26@i.softbank.jp>
2018-05-14 21:47:25 +0900

    5月14日

--- TestDB/template/sql/ecsite.sql
+++ TestDB/template/sql/ecsite.sql
@@ -1,24 +1,24 @@
-set namea utf8;
+set names utf8;
 set foreign_key_checks=0;
-drop database if exist ecsite;
+drop database if exists ecsite02;
 
-create database if not exist ecsite;
-create ecsite;
+create database if not exists ecsite02;
+use ecsite02;
 
-drop table if exist login_user_transaction;
+drop table if exists login_user_transaction;
 
-cretae table if exist login_user_transaction(
+create table login_user_transaction(
 id int not null primary key auto_increment,
-login_id varchar(16)unique,
+login_id varchar(16) unique,
 login_pass varchar(16),
 user_name varchar(50),
 insert_date datetime,
-updataed_date datetime
+updated_date datetime
 );
 
-drop table if exist item_info_transaction;
+drop table if exists item_info_transaction;
 
-create table item_info_transactin(
+create table item_info_transaction(
 id int  not null primary key auto_increment,
 item_name varchar(30),
 item_price int,
@@ -27,7 +27,7 @@ insert_date datetime,
 update_date datetime
 );
 
-drop table if exist user_buy_item_transaction;
+drop table if exists user_buy_item_transaction;
 
 create table user_buy_item_transaction(
 id int not null primary key auto_increment,
@@ -36,7 +36,7 @@ total_price int,
 total_count int,
 user_master_id varchar(16),
 pay varchar(30),
-insert date datetime,
+insert_date datetime,
 delete_date datetime
 );
 
