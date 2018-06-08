1398e25ad3560fe036be0d261608f06084d08084
salut822 <vaa.srts12@ezweb.ne.jp>
2018-06-08 21:23:38 +0900

    6月8日

--- TestDB/ecsite/src/com/internousdev/ecsite/action/UserCreateCompleteAction.java
+++ TestDB/ecsite/src/com/internousdev/ecsite/action/UserCreateCompleteAction.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.ecsite.dao.UserCreateCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserCreateCompleteAction extends ActionSupport implements SessionAware {
@@ -13,10 +14,10 @@ public class UserCreateCompleteAction extends ActionSupport implements SessionAw
 	private String loginPassword;
 	private String userName;
 	public Map<String,Object>session;
-	private UserCreateCompleteDAO userCreateCompleteDAO=new UserCreateCompleteDAO();
+	private UserCreateCompleteDAO UserCreateCompleteDAO=new UserCreateCompleteDAO();
 
 	public String execute() throws SQLException{
-		userCreateCompleteDAO.createUser(session.get("loginUserId").toString(),
+		UserCreateCompleteDAO.createUser(session.get("loginUserId").toString(),
 				session.get("loginPassword").toString(),
 				session.get("userName").toString());
 
