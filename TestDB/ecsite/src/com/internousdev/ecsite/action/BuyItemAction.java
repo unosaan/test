3b6cd8d21aa519b2645d253ed3380a3202a264d1
Nanasuzu0822 <nana_0822@ezweb.ne.jp>
2018-06-25 20:58:41 +0900

    6月25日

--- TestDB/ecsite/src/com/internousdev/ecsite/action/BuyItemAction.java
+++ TestDB/ecsite/src/com/internousdev/ecsite/action/BuyItemAction.java
@@ -4,25 +4,38 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
-import com.internousdev.ecsite.dao.BuyItemDAO;
-import com.internousdev.ecsite.dto.BuyItemDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BuyItemAction extends ActionSupport implements SessionAware{
 	public Map<String,Object>session;
+	private int count;
+	private String pay;
 
-	public String excute(){
-		String result="login";
-		if(session.containsKey("id")){
-			BuyItemDAO buyItemDAO=new BuyItemDAO();
-			BuyItemDTO buyItemDTO=buyItemDAO.getBuyItemInfo();
-			session.put("id", buyItemDTO.getId());
-			session.put("buyItem_name", buyItemDTO.getItemName());
-			session.put("buyItem_price", buyItemDTO.getItemPrice());
-
-		result=SUCCESS;
+	public String execute(){
+		String result=SUCCESS;
+		session.put("count", count);
+		int intCount=Integer.parseInt(session.get("count").toString());
+		int intPrice=Integer.parseInt(session.get("buyItem_price").toString());
+		session.put("total_price",intCount * intPrice);
+		String payment;
+		if(pay.equals("1")){
+			payment="現金払い";
+			session.put("pay",payment);
+		}else{
+			payment="クレジットカード";
+			session.put("pay",payment);
 		}
 		return result;
+	}
+
+
+
+	public void setCount(int count){
+		this.count=count;
+	}
+
+	public void setPay(String pay){
+		this.pay=pay;
 	}
 
 	@Override
--- TestDB/ecsite/src/com/internousdev/ecsite/action/BuyItemConfirmAction.java
+++ TestDB/ecsite/src/com/internousdev/ecsite/action/BuyItemConfirmAction.java
@@ -1,46 +1,34 @@
 package com.internousdev.ecsite.action;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.ecsite.dao.BuyItemCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
+
+
 
 public class BuyItemConfirmAction extends ActionSupport implements SessionAware {
 	public Map<String,Object>session;
-	private int count;
-	private String pay;
+	private BuyItemCompleteDAO buyItemCompleteDAO=new BuyItemCompleteDAO();
 
-	public String execute(){
+	public String execute() throws SQLException{
+		buyItemCompleteDAO.buyItemInfo(
+				session.get("id").toString(),
+				session.get("login_user_id").toString(),
+				session.get("total_price").toString(),
+				session.get("count").toString(),
+				session.get("pay").toString());
+
 		String result=SUCCESS;
-		session.put("count", count);
-		int intCount=Integer.parseInt(session.get("count").toString());
-		int intPrice=Integer.parseInt(session.get("buyItem_price").toString());
-		session.put("total_price",intCount * intPrice);
-		String payment;
-		if(pay.equals("1")){
-			payment="現金払い";
-			session.put("pay",payment);
-		}else{
-			payment="クレジットカード";
-			session.put("pay",payment);
-		}
 		return result;
-	}
 
-
-
-	public void setCount(int count){
-		this.count=count;
-	}
-
-	public void setPay(String pay){
-		this.pay=pay;
 	}
 
 	@Override
 	public void setSession(Map<String,Object>session){
 		this.session=session;
 	}
-
 }
